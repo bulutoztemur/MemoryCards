e629befeb72ebb4b7ea9785b9f7abe67bdf703bd
= <BulutOz@garantibbva.com.tr>
2021-02-20 13:27:52 +0300

    Initial Commit

---
+++ MemoryCards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoryCards
+//
+//  Created by Alaattin Bulut Öztemur on 20.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
